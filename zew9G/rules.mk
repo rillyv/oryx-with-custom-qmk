2e7c23f821d00796355592c9a4d1422608ce530b
rillyv <rillyv.dev@gmail.com>
2025-09-02 12:55:55 +0200

    will this work?

--- zew9G/rules.mk
+++ zew9G/rules.mk
@@ -1,8 +1,19 @@
-VCONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = no
-ORYX_ENABLE = yes
+# Make this firmware speak VIA
 VIA_ENABLE = yes
-RGB_MATRIX_CUSTOM_KB = yes
+
+# Your Oryx defaults that your keymap uses
 TAP_DANCE_ENABLE = yes
+ORYX_ENABLE = yes
+RGB_MATRIX_CUSTOM_KB = yes
+
+# Keep the rest as you had
+MOUSEKEY_ENABLE = no
 SPACE_CADET_ENABLE = no
+COMMAND_ENABLE = no
+VCONSOLE_ENABLE = no
+
+# Optional QoL
+LTO_ENABLE = no
+CONSOLE_ENABLE = no
+WERROR_ENABLE = no
+
